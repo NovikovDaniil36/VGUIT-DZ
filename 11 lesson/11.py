c610733765bf549213d0931bf92a16bc4c416ee9
NovikovDaniil36 <113294969+NovikovDaniil36@users.noreply.github.com>
2022-11-28 23:48:54 +0300

    Update 11 lesson

--- 11 lesson/11.py
+++ 11 lesson/11.py
@@ -8,6 +8,7 @@ from tkinter import *
 def request_repo():
   repo = 'https://api.github.com/repos/NixOS/nixpkgs'
   data = requests.get(repo).json()
+  data = dict((key, data[key]) for key in ['company', 'created_at', 'email', 'id', 'name', 'url'] if key in data)
   with open('repo.json', 'w') as file:
     json.dump(data, file)
 
